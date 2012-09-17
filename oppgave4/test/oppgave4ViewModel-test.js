efb83efdc19f7d31e5e2ce429141394aed603a2d
Jørn Are Hatlelid <jah@computas.com>
2012-09-17 11:06:58 +0200

    lint

--- oppgave4/test/oppgave4ViewModel-test.js
+++ oppgave4/test/oppgave4ViewModel-test.js
@@ -1,18 +1,18 @@
 "use strict";
 buster.testCase("The ViewModel", {
     "setUp": function () {
-    	this.vm = new ReservationsViewModel();
+        this.vm = new ReservationsViewModel();
     },
     "canReserveAnotherSeat should be defined": function () {
-    	assert.defined(this.vm.canReserveAnotherSeat);
+        assert.defined(this.vm.canReserveAnotherSeat);
     },
     "can reserve another seat when list is empty": function () {
-    	this.vm.seats([]);
-    	assert(this.vm.canReserveAnotherSeat());
+        this.vm.seats([]);
+        assert(this.vm.canReserveAnotherSeat());
     },
     "can not reserve another seat if list contains a seat with no name": function () {
-    	this.vm.addSeat();
-    	refute(this.vm.canReserveAnotherSeat(), "Should not be able to reserve another seat when empty seat is in list");
+        this.vm.addSeat();
+        refute(this.vm.canReserveAnotherSeat(), "Should not be able to reserve another seat when empty seat is in list");
     },
     "can reserve another seat if an empty seat is added and given a name": function () {
         var newseat = this.vm.addSeat();
