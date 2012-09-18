b174adfab23f7dba0a14f4d4f553adb4e9aa8ec8
jornare <jornare@bmenu.com>
2012-09-18 22:26:36 +0200

    some testcases for oppgave4

--- oppgave4svar/test/oppgave4ViewModel-test.js
+++ oppgave4svar/test/oppgave4ViewModel-test.js
@@ -7,7 +7,7 @@ buster.testCase("The ViewModel", {
     "setUp": function () {
         // Spying on existing method
         this.spy(MYAPP.services, 'load');
-        this.vm = new TaskListViewModel();
+        this.vm = new ReservationsViewModel();
     },
     "load should be called on initialization": function () {
         assert.calledOnce(MYAPP.services.load);
@@ -15,21 +15,35 @@ buster.testCase("The ViewModel", {
     "should have one element when initialized": function () {
         assert.same(this.vm.tasks().length, 1);
     },
-    "should call save when save command is called, but not before": function () {
+    "should call save when an empty seat is added": function () {
         // replacing method with spy
-        MYAPP.services.save = this.spy();
-        refute.calledOnce(MYAPP.services.save);
-        this.vm.save();
-        assert.calledOnce(MYAPP.services.save);
+        MYAPP.services.saveItem = this.spy();
+        refute.calledOnce(MYAPP.services.saveItem);
+        this.vm.addSeat();
+        assert.calledOnce(MYAPP.services.saveItem);
     },
-    "should have loaded testdata from stub at init": function () {
-        var firstTask = this.vm.tasks()[0];
-        assert.defined(firstTask, 'There should be a loaded task');
-        assert.equals(firstTask.title(), 'testtitle', 'The title should be title from the stub');
+    "should not call save when an existing seat is added": function () {
+        MYAPP.services.saveItem = this.spy();
+        refute.calledOnce(MYAPP.services.saveItem);
+        this.vm.addSeat(1,"Per",1);
+        refute.calledOnce(MYAPP.services.saveItem);
     },
-    "the save function should have the tasklist as the first argument": function () {
-        MYAPP.services.save = this.spy();
-        this.vm.save();
-        assert.calledWith(MYAPP.services.save, this.vm.tasks());
+    "should call save when a seat is modified": function () {
+        var seat;
+    	MYAPP.services.saveItem = this.spy();
+        refute.calledOnce(MYAPP.services.saveItem);
+        seat = this.vm.addSeat(1,"Per",1);
+        refute.calledOnce(MYAPP.services.saveItem);
+        seat.name("Petter");
+        assert.calledOnce(MYAPP.services.saveItem);
+    },
+    "should call removeItem when a seat is removed": function () {
+        var seat;
+    	MYAPP.services.removeItem = this.spy();
+        refute.calledOnce(MYAPP.services.removeItem);
+        seat = this.vm.addSeat(1,"Per",1);
+        refute.calledOnce(MYAPP.services.removeItem);
+        this.vm.removeSeat(seat);
+        assert.calledOnce(MYAPP.services.removeItem);
     }
 });
