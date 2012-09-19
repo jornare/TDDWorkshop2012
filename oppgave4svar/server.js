d2018e44bc8648c71b82a00191ff59904f13b953
Jørn Are Hatlelid <jah@computas.com>
2012-09-19 18:07:58 +0200

    update server.js temporary

--- oppgave4svar/server.js
+++ oppgave4svar/server.js
@@ -1,15 +1,39 @@
-var http = require('http'),
-    response = JSON.stringify([{'name':'Ola','meal':1},{'name':'Per','meal':2},{'name':'Eva','meal':3},{'name':'Olga','meal':3}]);
+var http = require('http')
+			, fs = require('fs')
+			, reservations = [{id:1, 'name':'Ola','meal':1},
+                    {id:2, 'name':'Per','meal':2},
+                    {id:3, 'name':'Eva','meal':3},
+                    {id:4, 'name':'Olga','meal':3}];
 
 // do not EVER use the death* for allow origin for real
 // It's just used here to avoid having to run the index of a local fileserver
 http.createServer(function (req, res) {
     if (req.method === "GET") {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'Access-Control-Allow-Origin': '*'
-        });
-        res.end(response);
+
+        console.log(req.url);
+        if(req.url.indexOf('.')>0){
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Access-Control-Allow-Origin': '*'
+            });        	
+	        fs.readFile(__dirname + req.url,
+	            function (err, data) {
+	                if (err) {
+	                    res.writeHead(400);
+	                    return res.end('File not found');
+	                }else{
+	                	return res.end((data+'').replace(/^\uFEFF/, ''));
+	                }
+	        });
+        }else{
+            res.writeHead(200, {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*'
+            });        	
+            res.end(JSON.stringify(reservations));
+        }
+
+
     } else {
          res.writeHead(201, {
             'Content-Type': 'text/plain',
