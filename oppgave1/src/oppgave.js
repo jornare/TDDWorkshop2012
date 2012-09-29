a8ff92743a6ee71738ea3c731399c0c925992077
jornare <jornare@bmenu.com>
2012-09-29 11:58:10 +0200

    added some notes

--- oppgave1/src/oppgave.js
+++ oppgave1/src/oppgave.js
@@ -1,3 +1,9 @@
+/*
+ * Dette er all kildekoden til oppgave 1
+ * Er alt i orden?
+ * Hvis ikke, husk å lage en test som vil feile før du korrigerer koden.
+ * Er dere 2 personer kan den ene lage en test som feiler, mens den andre fikser kildekoden.
+ */
 "use strict";
 var doubler = 2;
 
--- oppgave1/test/oppgave-test.js
+++ oppgave1/test/oppgave-test.js
@@ -1,12 +1,19 @@
+/*
+ * Oppgave 1
+ * Dette er testene til oppgave 1.
+ * Det er et par feil her, kan du finne de?
+ * Når du kjører "buster test" vil lint-feil vises i gult. Det er feil som har med hvor pent koden er satt opp å gjøre, ikke logiske feil.
+ */
+
 "use strict";
 require("buster");
 var my = require("../src/oppgave.js");
 
-buster.testCase("A module", {
-    "states the obvious": function () {
+buster.testCase("My Test Case", {
+    "The double of five should equal ten": function () {
 	assert.equals(my.double(5), "10", "Should double the number");
     },
-    "we need a funny failing test also": function () {
-	assert.same(my.double(5), "10", "Should double the number");
+    "The double of five should be the same as 10": function () {
+	assert.same(my.double(5), "10", "Double of 5 should be the same as 10");
     }
 });
